3ca5d5e26c8ba1bca061765314d35d291003659d
github username <github email address>
2018-11-28 12:53:25 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,56 +27,50 @@ class Owner
   end
 
   def say_species
-    "I am a #{@species}."
+    "I am a #{species}."
   end
 
   def buy_fish(name)
-    @pets[:fishes] << Fish.new(name)
+    pets[:fishes] << Fish.new(name)
   end
 
   def buy_cat(name)
-    @pets[:cats] << Cat.new(name)
+    pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
-    @pets[:dogs] << Dog.new(name)
+    pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
-    @pets[:dogs].each do |details|
-      if details.mood == "nervous"
-        details.mood = "happy"
-      end
+    pets[:dogs].each do |details|
+      details.mood = "happy"
     end
   end
 
   def play_with_cats
-    @pets[:cats].each do |details|
-      if details.mood == "nervous"
-        details.mood = "happy"
-      end
+    pets[:cats].each do |details|
+      details.mood = "happy"
     end
   end
 
   def feed_fish
-    @pets[:fishes].each do |details|
-      if details.mood == "nervous"
-        details.mood = "happy"
-      end
+    pets[:fishes].each do |details|
+      details.mood = "happy"
     end
   end
 
   def sell_pets
-    @pets.each do |type, array|
+    pets.each do |species, array|
       array.each do |id|
         id.mood = "nervous"
       end
+      array.clear
     end
-    @pets.clear
   end
 
   def list_pets
-   "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+   "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end
 
 end
